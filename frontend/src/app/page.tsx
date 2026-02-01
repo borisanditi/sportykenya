f4d45849e684985fc6008c592c109936021c37e2
francis-auka <francisauka7@gmail.com>
2026-02-01 10:59:48 +0300

    Remove Premier League table feature temporarily - awaiting suitable API solution

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,6 @@ import { HeroSection } from "@/components/home/HeroSection";
 import { NewsGrid } from "@/components/home/NewsGrid";
 import { Scoreboard } from "@/components/home/Scoreboard";
 import { LiveMatches } from "@/components/scorebat/LiveMatches";
-import { PremierLeagueTable } from "@/components/scorebat/PremierLeagueTable";
 import { client } from "@/lib/sanity";
 import { BREAKING_NEWS_QUERY, HERO_STORIES_QUERY, NEWS_GRID_QUERY } from "@/lib/queries";
 
@@ -21,18 +20,8 @@ export default async function Home() {
       <BreakingNews news={breakingNews} />
       <HeroSection stories={heroStories} />
       <Scoreboard />
-
-      {/* Main Content Area */}
-      <div className="container mx-auto px-4 py-8 grid grid-cols-1 lg:grid-cols-3 gap-8">
-        <div className="lg:col-span-2 space-y-8">
-          <LiveMatches />
-          <NewsGrid posts={gridPosts} />
-        </div>
-        <div className="space-y-8">
-          <PremierLeagueTable />
-          {/* Additional sidebar content can go here */}
-        </div>
-      </div>
+      <LiveMatches />
+      <NewsGrid posts={gridPosts} />
     </div>
   );
 }
